02bcc4e36e7df5dfe9d621696fc8056c14bb20af
Alex Vidal <alex.vidal@gmail.com>
2011-06-07 12:27:50 -0500

    Add permission functions to authentication backend.

--- organizations/backends.py
+++ organizations/backends.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.backends import ModelBackend
-from django.contrib.auth.models import check_password
+from django.contrib.auth.models import Permission
 
 from .models import Organization, OrganizationUser
 
 
 class OrganizationBackend(ModelBackend):
+
+    supports_object_permissions = True
 
     def authenticate(self, organization=None, username=None, password=None):
         try:
@@ -16,12 +18,96 @@ class OrganizationBackend(ModelBackend):
             user = OrganizationUser.objects.get(organization=organization,
                                                 username__iexact=username,
                                                 user__is_active=True)
+            if user.check_password(password):
+                return user
+
         except OrganizationUser.DoesNotExist:
             return None
 
-        valid_passwd = check_password(password, user.password)
+    def _create_permission_set(self, perms=None):
+        """
+        Expects a queryset of permissions, returns a formatted
+        set.
+        """
 
-        return user if valid_passwd else None
+        if perms is None:
+            return set()
+
+        perms = perms.values_list('content_type__app_label',
+                                    'codename').order_by()
+
+        return set(['%s.%s' % (ct, name) for ct, name in perms])
+
+
+    def get_group_permissions(self, user_obj, obj=None):
+        """
+        Returns a set of all permission strings that this user has through
+        his/her roles for the given object.
+
+        We use the `group` designator just for consistency.
+        """
+
+        # superusers get all permissions, like usual
+        if user_obj.is_superuser:
+            perms = Permission.objects.all()
+            return self._create_permission_set(perms)
+
+        # if the user is not an OrganizationUser, return the empty set
+        if not isinstance(user_obj, OrganizationUser):
+            return set()
+
+        # if the user is not in any roles, return the empty set
+        super_role_count = user_obj.super_roles.count()
+        if not any([super_role_count, user_obj.roles.count()]):
+            return set()
+
+        # first, get the perms provided by the super roles
+        perms = Permission.objects.filter(superrole__organizationuser=user_obj)
+
+        # now, check the regular role permissions if they are in the
+        # organization that owns the object
+        attname = getattr(obj, '_ORGANIZATION_ATTRIBUTE', 'organization')
+        obj_org = getattr(obj, attname, None)
+
+        if obj_org is None:
+            # If the object doesn't have an organization, they can't get
+            # any role-based permissions
+            pass
+        elif obj_org not in user_obj.get_all_organizations():
+            # If the user is not in the organization that owns the object,
+            # they can't get any role-based permissions
+            pass
+        else:
+            # Otherwise, the user is in the organization that owns the
+            # object, so check role permissions
+            roles = user_obj.roles
+            role_perms = Permission.objects.filter(role__in=roles)
+            perms = perms | role_perms
+
+        return self._create_permission_set(perms)
+
+    def get_all_permissions(self, user_obj, obj=None):
+        if user_obj.is_anonymous():
+            return set()
+
+        # we don't support user permissions
+        return self.get_group_permissions(user_obj, obj=obj)
+
+    def has_perm(self, user_obj, perm, obj=None):
+        if not user_obj.is_active:
+            return False
+
+        return perm in self.get_all_permissions(user_obj, obj=obj)
+
+    def has_module_perms(self, user_obj, app_label, obj=None):
+        if not user_obj.is_active:
+            return False
+
+        for perm in self.get_all_permissions(user_obj, obj=obj):
+            if perm[:perm.index('.')] == app_label:
+                return True
+
+        return False
 
     def get_user(self, user_id):
         try:
