5328e3113e7ade4c8f0125decb87061d87e1bdbe
Alex Vidal <alex.vidal@gmail.com>
2011-06-21 11:34:31 -0500

    Add basic setup.py script.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(name='django-organizations',
+      version='0.1',
+      description='Organization support for Django',
+      author='Alex Vidal',
+      author_email='alex.vidal@gmail.com',
+      url='https://github.com/avidal/django-organizations',
+      packages=['organizations'])
